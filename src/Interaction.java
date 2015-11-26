60fb86675a4893adab10598d607fd71598d6c1dc
ilyes hammadi <hammadiilyesahmed@gmail.com>
2015-11-26 13:07:01 +0100

    update interaction creation automate and add check word fonctionaliter

--- src/Interaction.java
+++ src/Interaction.java
@@ -1,5 +1,3 @@
-import com.sun.org.apache.xpath.internal.SourceTree;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
@@ -38,6 +36,7 @@ public class Interaction {
                 case 2:
                     // Cheking a word with the automata
                     // that was define in the first choice
+                    interactionCheckWord();
                     break;
                 case 3:
                     iteractionPrintAutomate();
@@ -49,6 +48,25 @@ public class Interaction {
 
 
     }
+
+    private static void interactionCheckWord() {
+        String word = null;
+
+        if (!automate.isEmpty()) {
+            System.out.print("Saisir le mot a tester : ");
+            word = key.next();
+
+            if (automate.isValideWord(word)) {
+                System.out.println("se mot appartient au langage definit par cette automate");
+            } else {
+                System.out.println("se mot né pas reconu par cette automate");
+            }
+        } else {
+            System.out.println("l'automate na pas encore été definit");
+        }
+
+    }
+
 
     private static void iteractionPrintAutomate() {
         if (!automate.isEmpty()) {
@@ -66,28 +84,8 @@ public class Interaction {
         boolean[] finalStates = null;
 
 
-        // Saisi de l'aplhabet par the user
-        String input;
-        System.out.println("Saisir l'alphabet : ");
+        inputAlphabet(alphabetList);
 
-        // Partie input letters
-        do {
-            // Read a letter
-            System.out.print(">> ");
-            input = key.next();
-
-            // Get the char from the String
-            char actualCharInput = input.charAt(0);
-
-            // Cheking if the input is a letter
-            // if it's not so print an ERROR
-            if ('a' <= actualCharInput && actualCharInput <= 'z' || Objects.equals(input, "-1")) {
-                alphabetList.add(input);
-            } else {
-                System.out.println("ERROR please enter only letters");
-            }
-
-        } while (!Objects.equals(input, "-1"));
 
         // Saisi du nombre de state
         System.out.print("Qu'elle est le nombres d'etats : ");
@@ -122,7 +120,7 @@ public class Interaction {
 
         // Lecture des etats final
         int indiceInput;
-        System.out.println("Qu'elle sont les etats finaux : ");
+        System.out.println("Qu'elle sont les etats finaux : (-1 pour terminer)");
         do {
             indiceInput = key.nextInt();
             if (indiceInput > 0) {
@@ -145,9 +143,9 @@ public class Interaction {
 
                 // Testing the user input
                 // before add in it into thr states array
-                if (value > 0 && value <= stateNumbers) {
+                if (value >= 0 && value <= stateNumbers) {
                     // insertion into the states arrays
-                    states[i][j] = value;
+                    states[i][j] = value - 1;
                     j++;
                 } else {
                     System.out.println("ERROR le nombre que vous avez entrer né pas un etas de cette automate");
@@ -158,6 +156,31 @@ public class Interaction {
 
         System.out.println("L'Automate a été parfaitement crée");
 
+    }
+
+    private static void inputAlphabet(List<String> alphabetList) {
+        // Saisi de l'aplhabet par the user
+        String input;
+        System.out.println("Saisir l'alphabet : (-1 pour terminer)");
+
+        // Partie input letters
+        do {
+            // Read a letter
+            System.out.print(">> ");
+            input = key.next();
+
+            // Get the char from the String
+            char actualCharInput = input.charAt(0);
+
+            // Cheking if the input is a letter
+            // if it's not so print an ERROR
+            if ('a' <= actualCharInput && actualCharInput <= 'z' || Objects.equals(input, "-1")) {
+                alphabetList.add(input);
+            } else {
+                System.out.println("ERROR please enter only letters");
+            }
+
+        } while (!Objects.equals(input, "-1"));
     }
 
 
